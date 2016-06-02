7de05d9e7855bee5a649155fd3542efc9de3f227
lin <Administrator@VURUFDLZHLRRCMQ>
2016-06-02 11:25:03 +0800

    helloWord

---
+++ src/org/greenrobot/eventbus/demo/Event.java
@@ -0,0 +1,23 @@
+package org.greenrobot.eventbus.demo;
+
+import java.util.List;
+
+public class Event
+{
+	/** 列表加载事件 */
+	public static class ItemListEvent
+	{
+		private List<Item> items;
+
+		public ItemListEvent(List<Item> items)
+		{
+			this.items = items;
+		}
+
+		public List<Item> getItems()
+		{
+			return items;
+		}
+	}
+
+}
